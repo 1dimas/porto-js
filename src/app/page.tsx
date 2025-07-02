04a4c3ee3f0befad31f6cfb64b08b9275c62a38b
1dimas <dimasdwianandaputra@gmail.com>
2025-07-02 19:24:45 +0700

    coba pertama upload

---
+++ src/app/page.tsx
@@ -0,0 +1,17 @@
+import Lanyard from "@/components/Lanyard/Lanyard";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen overflow-x-hidden">
+      <div className="container border mx-auto h-screen">
+        <div className="grid grid-cols-12">
+          <div className="col-span-6">A</div>
+          <div className="col-span-6">
+            <Lanyard />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
