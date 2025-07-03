1a68d0a8adc7d7a2c38aafedb57f6133e883049b
1dimas <dimasdwianandaputra@gmail.com>
2025-07-03 20:31:52 +0700

    Update file lanyard dan page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="min-h-screen overflow-x-hidden">
+    <div className="min-h-screen overflow-x-hidden bg-[#0A1021]">
       <div className="container border mx-auto h-screen">
         <div className="grid grid-cols-12">
-          <div className="col-span-6">A</div>
+          <div className="col-span-6"></div>
           <div className="col-span-6">
-            <Lanyard />
+            <Lanyard  position={[0, 0, 15]} gravity={[0, -40, 0]} />
           </div>
         </div>
       </div>
